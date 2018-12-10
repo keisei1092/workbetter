7e7ad45fb076f412bc0d0a1dd3b4e0720bf9cbb1
keisei1092 <keiseis1092@gmail.com>
2018-12-10 23:55:23 +0900

    一通りのCRUDができるようになった

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/edit'
-  get 'tasks/show'
-  get 'tasks/new'
+  get 'tasks/',         to: 'tasks#index',   as: 'tasks_index'
+  get 'tasks/new',      to: 'tasks#new',     as: 'tasks_new'
+  get 'tasks/:id/edit', to: 'tasks#edit',    as: 'tasks_edit'
+  post 'tasks/',        to: 'tasks#create',  as: 'tasks_create'
+  post 'tasks/:id',     to: 'tasks#update',  as: 'tasks_update'
+  get 'tasks/:id',      to: 'tasks#show',    as: 'tasks_show'
+  delete 'tasks/:id/destroy',   to: 'tasks#destroy', as: 'tasks_destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'tasks#index'
