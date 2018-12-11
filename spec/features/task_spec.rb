c5040040e1585ada9abedcde19c4a0b3801f6edc
keisei1092 <keiseis1092@gmail.com>
2018-12-11 11:28:56 +0900

    タスク作成specがかけた

---
+++ spec/features/task_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Task', :type => :feature do
+  scenario 'Create a new task' do
+    visit '/tasks/new'
+
+    fill_in 'task_name', :with => 'My Task'
+    fill_in 'task_detail', :with => 'Detail'
+
+    click_button 'Save Task'
+
+    expect(page).to have_text('タスクを作成しました')
+  end
+end
