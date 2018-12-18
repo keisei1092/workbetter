a3024111fd6a2837a327db6335fe82695829be5b
keisei1092 <keiseis1092@gmail.com>
2018-12-19 04:02:40 +0900

    add due_date to strong_params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,6 +47,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :detail)
+    params.require(:task).permit(:name, :detail, :due_date)
   end
 end
